e48247207f691c9f54006aa4036ee9502a16834a
Major&Hannah <majorx234@googlemail.com>
2021-02-06 23:13:52 +0100

    Implement loginfunction to validate user

--- resources/user_accounts.py
+++ resources/user_accounts.py
@@ -3,6 +3,9 @@ from flask import Flask, jsonify, request
 from http import HTTPStatus
 
 users = []
+myuser = {'username':'TestUser', 'password': 'dd377093c6a6e841a49695cf11b440fb'}
+users.append(myuser)
+
 api = Namespace('user_accounts',
                 description="user accounts related operations")
 
@@ -12,12 +15,30 @@ class UserAccounts(Resource):
         '''Erstelle einen UserAccount'''
         new_user_json = request.json
         print(new_user_json)      
-        id = len(users)
-        print("my id: " + str(id))
-        users.append(new_user_json)
-        return new_user_json, HTTPStatus.CREATED
-
+        #id = len(users)
+        #print("my id: " + str(id))
+        #users.append(new_user_json)
+        #return new_user_json, HTTPStatus.CREATED
+        username = new_user_json["username"]
+        password = new_user_json["password"]
+        print("username: {}".format(username))
+        print("password: {}".format(password))
+        if ( self.logTheUserIn(users,new_user_json)):
+            print("ok")
+            return new_user_json, HTTPStatus.OK
+        else:
+            print("computer says no!")
+            return new_user_json, HTTPStatus.FORBIDDEN
+           
     def get(self):
         '''Gebe alle erstellten User zurück'''
         return users, HTTPStatus.OK
-    
+
+    def logTheUserIn(self, users, newuser):
+       #Vergleich mit Passworthash einbauen
+        for user in users:
+            if  (user["username"] == newuser["username"]) :
+                return True
+        print("der user is nicht drin")    
+        return False  # hier hatter die ganze schleife
+
